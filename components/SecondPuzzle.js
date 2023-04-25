728e8d4478760b2402a04dd7afe9ceb8beb0533c
kalifayouxel <hkhalifa@youxel.com>
2023-04-26 00:42:22 +0200

    second puzzle logic finished

--- components/SecondPuzzle.js
+++ components/SecondPuzzle.js
@@ -1,71 +1,93 @@
-import { StyleSheet, Text, View  } from "react-native"
-import {Svg, Path, G} from 'react-native-svg';
-import {Africa} from "../worldmaps/africa"
+import { Button, StyleSheet, Text, View } from "react-native"
+import { Svg, Path, G } from 'react-native-svg';
+import { Africa } from "../worldmaps/africa"
 import { useRandom } from "../hooks/useRandom";
 import { useEffect, useRef, useState } from "react";
 
-export default function SecondPuzzle(){
+export default function SecondPuzzle() {
 
-    var continent = Africa;
-    var [previousIndexes, getNextIndex, getInitIndex] = useRandom(continent.countries.length);
-    const [puzzleState, setPuzzleState] = useState({score: 0, randomIndex: getInitIndex()})
+  var continent = Africa;
+  var [previousIndexes, getNextIndex, getInitIndex, generateAndShuffleChoices] = useRandom(continent.countries.length);
+  const [puzzleState, setPuzzleState] = useState({ score: 0, randomIndex: 0 })
 
-    const randomCountryId = useRef()
-    
-    const countryPress = (cId)  => {
-        var nextState = puzzleState;
-        if(cId == randomCountryId.current){
-            console.log('success');
-            var nextScore = nextState.score + 1;
-            nextState = {...nextState, score: nextScore};
-        }
-        else{
-            console.log('fail');
-        }
-        var nextRandomIndex = getNextIndex();
-        nextState = {...nextState, randomIndex: nextRandomIndex};
-        setPuzzleState(nextState);
-      }
+  const randomCountryId = useRef()
+  const choices = generateAndShuffleChoices(puzzleState.randomIndex);
 
-      console.log('rerender random:'+puzzleState.randomIndex);
-    return(
-        <View style={styles.container}>
-      <Text style={styles.conuntryHeader}>you score is: {puzzleState.score}/{previousIndexes.length}({continent.countries.length})</Text>
+  useEffect(() => {
+    setPuzzleState({ score: 0, randomIndex: getInitIndex() })
+  },[])
+
+  const countryPress = (cId) => {
+    var nextState = puzzleState;
+    if (cId == randomCountryId.current) {
+      console.log('success');
+      var nextScore = nextState.score + 1;
+      nextState = { ...nextState, score: nextScore };
+    }
+    else {
+      console.log('fail');
+    }
+    var nextRandomIndex = getNextIndex();
+    nextState = { ...nextState, randomIndex: nextRandomIndex };
+    setPuzzleState(nextState);
+  }
+
+  if (puzzleState.randomIndex == -1) {
+    return (
+      <View style={styles.puzzlePage}>
+        <Text style={styles.conuntryHeader}>you scored {puzzleState.score}/{previousIndexes.length}</Text>
+      </View>
+    )
+  }
+  return (
+    <View style={styles.puzzlePage}>
+      <Text style={styles.conuntryHeader}>{puzzleState.score}/{(previousIndexes.length - 1)}({continent.countries.length})</Text>
+
+
+      <View style={styles.choicesWrapper}>
+        {choices.map(item => {
+          return (<Button onPress={() => countryPress(continent.countries[item].id)} title={continent.countries[item].en_name} key={"choice_" + continent.countries[item].id} />)
+        })}
+      </View>
       <Svg>
         <G scale={0.42} width={continent.width} height={continent.height}>
           {continent.countries.map((cntry, index) => {
-            if(index == puzzleState.randomIndex){
-                randomCountryId.current = cntry.id;
+            if (index == puzzleState.randomIndex) {
+              randomCountryId.current = cntry.id;
             }
             return (
               <Path
                 key={index}
                 stroke="black"
                 strokeWidth={2}
-                fill= { index == puzzleState.randomIndex?  "gray": "silver"}
+                fill={index == puzzleState.randomIndex ? "gray" : "silver"}
                 d={cntry.d}
-                onPress={() => countryPress(cntry.id)}
               />
             );
           })}
         </G>
       </Svg>
+
     </View>
-    )
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      alignContent:'center',
-      marginTop:200
-    },
-    conuntryHeader:{
-      marginBottom:15,
-      alignContent:'flex-start'
-    }
-  });
-  
+  puzzlePage: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignContent: 'center',
+    marginTop: 200
+  },
+  conuntryHeader: {
+    marginBottom: 15,
+    alignContent: 'flex-start'
+  },
+  choicesWrapper: {
+    justifyContent: 'space-between',
+    alignItems: 'flex-start'
+  }
+
+});
--- hooks/useRandom.js
+++ hooks/useRandom.js
@@ -1,43 +1,53 @@
 import { useRef } from "react";
 
-export function useRandom(maxLength){
+export function useRandom(maxLength) {
     const indexVals = useRef([]);
 
     const generateRandom = () => {
-        var randNum = Math.round (Math.random() * 100);
-        var randIndex = Math.round (randNum * maxLength / 100);
+        var randNum = Math.round(Math.random() * 100);
+        var randIndex = Math.round(randNum * maxLength / 100);
         randIndex = randIndex % maxLength;
         return randIndex;
     };
 
     const getNextIndex = () => {
         var previousValues = indexVals.current;
-         if(previousValues.length == maxLength)
+        if (previousValues.length == maxLength)
             return -1;
         var nextRand = generateRandom();
-        while(previousValues.findIndex(e => e === nextRand) > -1){
+        while (previousValues.findIndex(e => e === nextRand) > -1) {
             nextRand++;
             nextRand = nextRand % maxLength;
-        } 
-        indexVals.current = ([...previousValues,nextRand]); 
+        }
+        indexVals.current = ([...previousValues, nextRand]);
         return nextRand;
     };
 
     const getInitIndex = () => {
-        if(indexVals.current.length == 0){
+        if (indexVals.current.length == 0) {
             var initIndex = generateRandom();
-            indexVals.current = ([initIndex]); 
+            indexVals.current = ([initIndex]);
             return initIndex;
         }
     }
 
     const generateAndShuffleChoices = (currentIndex) => {
-        var choices = new Array(4);
-        choices[0] = currentIndex // correct answer
-        for (var i = 1; i <= 3; i++){
-            var temp = generateRandom();
+        var choices = [currentIndex] // correct answer
+
+        for (var i = 0; i < 3; i++) {
+            var choice = generateRandom();
+            while (choices.findIndex(e => e === choice) > -1) {
+                choice++;
+                choice = choice % maxLength;
+            }
+            choices.push(choice);
         }
+
+        var shuffle = generateRandom() % 4;
+        for (var i = 0; i <= shuffle; i++)
+            choices.push(choices.shift());//shift the array
+        return choices;
     }
-     
-    return [indexVals.current, getNextIndex, getInitIndex]
+
+    return [indexVals.current, getNextIndex, getInitIndex, generateAndShuffleChoices]
 }
